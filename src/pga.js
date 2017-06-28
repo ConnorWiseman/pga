cb54a97598eb1715dc6982d9d9c0fc579893209f
Connor Wiseman <wiseman.connor@gmail.com>
2017-06-27 23:26:39 -0600

    Attempted support for string literal interpolation to sql tagging function

--- src/pga.js
+++ src/pga.js
@@ -175,9 +175,20 @@ let PostgreSQLAdapter = module.exports = class PostgreSQLAdapter {
    *    });
    */
   sql(strings, ...keys) {
+    let params = keys.slice();
+
     return {
-      text:   strings.reduce((sql, frag, i) => sql + '$' + i + frag),
-      values: keys
+      text: strings.reduce((sql, frag, i) => {
+        let literal;
+
+        if (sql.charAt(sql.length - 1) === '.') {
+          literal = params.splice(i, 1)[0];
+          return sql + literal + frag;
+        }
+        
+        return sql + '$' + i + frag
+      }),
+      values: params
     };
   }
 
